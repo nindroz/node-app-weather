3240fb3761e0fa08fc9b3c58b535d24e5af62cf8
hitesh <mantha.hitesh@gmail.com>
2020-07-02 21:42:47 -0700

    changed to axios from http so i could use async

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const config = require("./config.js")
 const bp = require("body-parser")
 const http = require("http")
@@ -10,21 +11,16 @@ app.get('/', (req, res) => {
     res.render("index")
 })
 
-app.post('/', (req, res) => {
-    res.render("index") 
-    
-    let link = `http://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=${config.config.MY_KEY}`
-    http.get(link, (resp) => {
-        let data = ''
-        resp.on('data', (chunk) =>{
-            data += chunk
+post('/', (req,res) => {
+    res.render('index')
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=${config.config.MY_KEY}`)
+        .then(response => {
+            console.log(response.data.main.temp)
         })
-        resp.on('end', () => {
-            console.log(JSON.parse(data).main.temp)
+        .catch(error => {
+            console.log(error)
         })
-    })
 })
-
 
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`);
